83b8ecd38973730a871a10a0bd06c6d63f0f79b6
Dominic Chapman <dominicchapman@me.com>
2020-05-19 19:14:19 +0100

    Update todo list layout

--- examples/todo-list/src/index.tsx
+++ examples/todo-list/src/index.tsx
@@ -155,38 +155,59 @@ export const App = () => {
   };
 
   return (
-    <div
-      css={{
-        alignItems: "flex-start",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <div css={{ display: "flex" }}>
+    <div css={{ alignItems: "flex-start", display: "flex" }}>
+      <div css={{ display: "flex", marginRight: 8 }}>
         <button onClick={clearStorage}>Clear</button>
       </div>
-      {nodes.map((node) => (
-        <Node key={node.nodeId} nodeId={node.nodeId} handleSync={sync} />
-      ))}
-      {getHostedDB() &&
-        getHostedDBMessages() &&
-        !!getHostedDBMessages().length && (
-          <div
-            css={{
-              border: "2px solid #0670de",
-              borderRadius: 4,
-              display: "flex",
-              flexDirection: "column",
-              padding: 12,
-            }}
-          >
-            {getHostedDBMessages()
-              .filter((el: IMessage) => el.table === LOCAL_TODOS)
-              .map((todo: IMessage) => {
-                return <div key={todo.row}>{todo.value}</div>;
-              })}
-          </div>
-        )}
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          marginLeft: 8,
+          marginRight: 8,
+        }}
+      >
+        {nodes.map((node) => (
+          <Node key={node.nodeId} nodeId={node.nodeId} handleSync={sync} />
+        ))}
+      </div>
+      <div css={{ marginLeft: 8 }}>
+        {getHostedDB() &&
+          getHostedDBMessages() &&
+          !!getHostedDBMessages().length && (
+            <div
+              css={{
+                border: "2px solid #0670de",
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                padding: 12,
+              }}
+            >
+              {getHostedDBMessages()
+                .filter((el: IMessage) => el.table === LOCAL_TODOS)
+                .map((todo: IMessage) => {
+                  return (
+                    <div
+                      key={todo.row}
+                      css={{
+                        border: "2px solid #C0C8D2",
+                        borderRadius: 4,
+                        padding: 12,
+                        "&:not(:last-child)": {
+                          marginBottom: 8,
+                        },
+                      }}
+                    >
+                      <div css={{ fontSize: 10 }}>{todo.row}</div>
+                      <div css={{ fontSize: 10 }}>{todo.timestamp}</div>
+                      <div>{todo.value}</div>
+                    </div>
+                  );
+                })}
+            </div>
+          )}
+      </div>
     </div>
   );
 };
